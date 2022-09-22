4c542d9aa6035a0440bb7dd6da5c2078f8b5e5ca
GiuBi2 <giulia.santos@estudante.ifms.edu.br>
2022-09-22 13:31:41 -0400

    first  commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', e => {
+    "use strict";//restrito a funcionar em navegadores comES6 >
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("./sw.js");
+    }
+}); 
+let botaoCalcular = document.getElementById("btn_calcular");
+
+function calcularpresenca(){
+   var nome = document.getElementById("name").value;
+   var cargahr = document.getElementById("carga").value;
+   var dias = document.getElementById("aulas").value;
+   var falta =document.getElementById("falta").value;
+let resultado;
+
+   var suafalta = cargahr*0.25;
+   var faltas = suafalta - falta;
+
+  
+if(falta > suafalta){
+    resultado = "Você está reprovado por falta em " + nome + ` , não importa se vai faltar mais`;
+} else {
+    resultado = "Você está em curso em " + nome + " , pode faltar " + faltas + " aulas " ;
+}
+document.getElementById("resultado").textContent = resultado
+}
+btn_calcular.addEventListener('click', calcularpresenca);
